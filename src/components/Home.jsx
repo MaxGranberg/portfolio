e7f73cae79bee3aa81b03241cf7a0bc113cfc24e
Max Granberg <mg224em@student.lnu.se>
2024-07-05 14:02:52 +0200

    add profile picture to home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Home.css'; // Import the custom CSS
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-      <h1 className="text-5xl md:text-6xl font-bold mb-4 mt-48">Välkommen till min portfolio!</h1>
-      <p className="text-xl md:text-1xl">Här får du reda på lite mer om mig och vilka projekt jag skapat & jobbar med!</p>
+      <div className="flex flex-col items-center mt-12">
+        <div className="w-32 h-32 md:w-48 md:h-48 rounded-full overflow-hidden shadow-lg mb-4">
+          <img
+            src="/profilbild.jpg"
+            alt="Your Name"
+            className="w-full h-full object-cover custom-object-position custom-zoom" // Use custom class for fine adjustment
+          />
+        </div>
+        <h1 className="text-5xl md:text-6xl font-bold mb-4">Välkommen till min portfolio!</h1>
+        <p className="text-xl md:text-2xl text-center">
+          Här får du reda på lite mer om mig och vilka projekt jag skapat & jobbar med!
+        </p>
+      </div>
     </div>
   );
 };
