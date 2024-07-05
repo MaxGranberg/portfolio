0a91213a9e5e13cdb0e6b782b6600e788be4728a
Max Granberg <mg224em@student.lnu.se>
2024-07-05 13:29:59 +0200

    update text to swedish

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="flex flex-col items-center h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-      <h1 className="text-5xl md:text-6xl font-bold mb-4 mt-48">Welcome to My Portfolio</h1>
-      <p className="text-xl md:text-2xl">Showcasing my projects and skills</p>
+      <h1 className="text-5xl md:text-6xl font-bold mb-4 mt-48">Välkommen till min portfolio!</h1>
+      <p className="text-xl md:text-1xl">Här får du reda på lite mer om mig och vilka projekt jag skapat & jobbar med!</p>
     </div>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,13 +6,13 @@ const NavBar = () => {
     <nav className="w-full">
       <ul className="flex justify-center space-x-6 md:space-x-8">
         <li>
-          <Link to="/" className="text-white text-lg md:text-xl hover:text-gray-300">Home</Link>
+          <Link to="/" className="text-white text-lg md:text-xl hover:text-gray-300">Start</Link>
         </li>
         <li>
-          <Link to="/about" className="text-white text-lg md:text-xl hover:text-gray-300">About</Link>
+          <Link to="/about" className="text-white text-lg md:text-xl hover:text-gray-300">Om mig</Link>
         </li>
         <li>
-          <Link to="/projects" className="text-white text-lg md:text-xl hover:text-gray-300">Projects</Link>
+          <Link to="/projects" className="text-white text-lg md:text-xl hover:text-gray-300">Projekt</Link>
         </li>
       </ul>
     </nav>
